c9d35861cb602d67be0dc1e2d2618b7b7ecd8728
senmaran <maran.happysanz@gmail.com>
2017-09-22 10:50:53 +0530

    Organizer Events
    Create and View

--- application/controllers/Organizer.php
+++ application/controllers/Organizer.php
@@ -10,11 +10,9 @@ class Organizer extends CI_Controller
 		  $this->load->helper('url');
 		  $this->load->library('session');
 		  $this->load->model('organizermodel');
-		  
-		  
        }
-
-//-------------------------City Add / Update---------------------------------
+	   
+//----------------------------------------------------------
 
      public function index()
 	 {
@@ -30,35 +28,133 @@ class Organizer extends CI_Controller
 	 		 }
 	 }
 
+//----------------------------------------------------------
+
+    function get_times( $default = '10:00', $interval = '+15 minutes' ) 
+	{
+		$output = '';
+		$current = strtotime( '00:00:00' );
+		$end = strtotime( '23:59:00' );
+		while( $current <= $end ) {
+			$time = date( 'H:i:s', $current );
+			$sel = ( $time == $default ) ? ' selected' : '';
+			$output .= "<option value=\"{$time}\">" . date( 'h.i A', $current ) .'</option>';
+			$current = strtotime( $interval, $current );
+		}
+		return $output;
+    }
+	
+
+//----------------------------------------------------------
+	 
+    public function get_city_name()
+     {
+	   	 $country_id = $this->input->post('country_id');
+		 $data['res']= $this->organizermodel->get_cityname($country_id);
+		 echo json_encode( $data['res']);
+     }
+//----------------------------------------------------------
    
      public function createevents()
 	 {
-	 	$datas = $this->session->userdata();
-	    $user_id = $this->session->userdata('id');
-	    $user_role = $this->session->userdata('user_role');
+	 	$datas=$this->session->userdata();
+	    $user_id=$this->session->userdata('id');
+	    $user_role=$this->session->userdata('user_role');
+
+	    $datas['country_list'] = $this->organizermodel->get_country();
+	    $datas['category_list'] = $this->organizermodel->get_category();
 
 		if($user_role==2)
 		{
-		  $this->load->view('organizer/create_events');
+		  $this->load->view('header');
+		  $this->load->view('organizer/create_events',$datas);
+		  $this->load->view('footer');
 	 	}else{
 	 			redirect('/');
 	 		 }
 	 }
-	
+
+//----------------------------------------------------------
+
      public function inserteevents()
 	 {
 	 	$datas = $this->session->userdata();
 	    $user_id = $this->session->userdata('id');
 	    $user_role = $this->session->userdata('user_role');
 		
+	    $event_name = $this->db->escape_str($this->input->post('event_name'));
+        $category = $this->input->post('category');
+        $country = $this->input->post('country');
+        $city = $this->input->post('city');
+        $venue = $this->input->post('venue');
+        $address = $this->db->escape_str($this->input->post('address'));
+        $description = $this->db->escape_str($this->input->post('description'));
+        $eventcost = $this->input->post('eventcost');
+        $sdate = $this->input->post('start_date');
+		$dateTime = new DateTime($sdate);
+		$start_date = date_format($dateTime,'Y-m-d');
+        $edate = $this->input->post('end_date');
+        $dateTime = new DateTime($edate);
+		$end_date = date_format($dateTime,'Y-m-d');
+        $start_time = $this->input->post('start_time');
+        $end_time = $this->input->post('end_time');
+        $txtLatitude = $this->input->post('txtLatitude');
+        $txtLongitude = $this->input->post('txtLongitude');
+        $pcontact_cell = $this->input->post('pcontact_cell');
+        $scontact_cell = $this->input->post('scontact_cell');
+        $contact_person = $this->input->post('contact_person');
+        $email = $this->input->post('email');
+        
+        $event_pic = $_FILES['eventbanner']['name']; 
+		$event_banner = trim($event_pic);
+		$uploaddir = 'assets/events/banners/';
+		$profilepic = $uploaddir.$event_banner;
+		move_uploaded_file($_FILES['eventbanner']['tmp_name'],$profilepic);
+
+        $eadv_status = $this->input->post('eadv_status');
+		$booking_sts = $this->input->post('booking_sts');
+		$hotspot_sts = $this->input->post('hotspot_sts');
+        $colour_scheme = $this->input->post('colour_scheme');
+		$event_status = $this->input->post('event_status');
+
+        $datas = $this->organizermodel->create_events($event_name,$category,$country,$city,$venue,$address,$description,$eventcost,$start_date,$end_date,$start_time,$end_time,$txtLatitude,$txtLongitude,$pcontact_cell,$scontact_cell,$contact_person,$email,$event_banner,$colour_scheme,$event_status,$eadv_status,$booking_sts,$hotspot_sts,$user_id,$user_role);
+        
+		$sta = $datas['status'];
+
+	    if($sta=="success"){
+			$this->session->set_flashdata('msg','Added Successfully');
+			redirect('organizer/view_events');
+	     }else if($sta=="Already Exist"){
+			$this->session->set_flashdata('msg','Already Exist');
+			redirect('organizer/view_events');
+	     }
+	     else{
+			$this->session->set_flashdata('msg','Faild To Add');
+			redirect('organizer/view_events');
+	     }
+	 }
+
+//----------------------------------------------------------
+	 
+	 public function viewevents()
+	 {
+	 	$datas = $this->session->userdata();
+	    $user_id = $this->session->userdata('id');
+	    $user_role = $this->session->userdata('user_role');
+
+		$datas['result'] = $this->organizermodel->list_events($user_id);
+		
 		if($user_role==2)
 		{
-			$category =$this->input->post("cboCategory");
-			$datas = $this->organizermodel->create_events($category,$user_id,$user_role);
-			//exit;
-	 	} else {
-		}
+		  $this->load->view('header');
+		  $this->load->view('organizer/view_events',$datas);
+		  $this->load->view('footer');
+	 	}else{
+	 			redirect('/');
+	 	}
 	 }
+//----------------------------------------------------------	 
+	 
 	 
 	 public function updateevents()
 	 {
@@ -68,26 +164,16 @@ class Organizer extends CI_Controller
 
 		if($user_role==2)
 		{
-		  $this->load->view('organizer/update_events');
+		  $this->load->view('header');
+		  $this->load->view('organizer/update_events',$datas);
+		  $this->load->view('footer');
 	 	}else{
 	 			redirect('/');
 	 		 }
 	 }
 
 
-	 public function listevents()
-	 {
-	 	$datas = $this->session->userdata();
-	    $user_id = $this->session->userdata('id');
-	    $user_role = $this->session->userdata('user_role');
 
-		if($user_role==2)
-		{
-		  $this->load->view('organizer/list_events');
-	 	}else{
-	 			redirect('/');
-	 		 }
-	 }
 
 }
 ?>
---
+++ application/views/organizer/view_events.php
@@ -0,0 +1,169 @@
+<div class="content-page"> <!-- Footer Close-->
+<!-- Start content -->
+<div class="content">
+    <!-- Top Bar Start -->
+  <div class="topbar">
+      <nav class="navbar-custom">
+         <ul class="list-inline float-right mb-0">
+            <li class="list-inline-item dropdown notification-list">
+               <a class="nav-link dropdown-toggle arrow-none waves-effect" data-toggle="dropdown" href="#" role="button"
+                  aria-haspopup="false" aria-expanded="false">
+               <i class="ion-ios7-bell noti-icon"></i>
+               <span class="badge badge-success noti-icon-badge">3</span>
+               </a>
+            </li>
+            <li class="list-inline-item dropdown notification-list">
+            <a class="nav-link dropdown-toggle arrow-none waves-effect nav-user" data-toggle="dropdown" href="#" role="button"
+               aria-haspopup="false" aria-expanded="false">
+            <img src="<?php echo base_url(); ?>assets/images/admin/admin.png" alt="user" class="rounded-circle">
+            </a>
+            <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
+            <a class="dropdown-item" href="#"><i class="mdi mdi-account-circle m-r-5 text-muted"></i> Profile</a>
+            <a class="dropdown-item" href="#"><span class="badge badge-success pull-right">5</span><i class="mdi mdi-settings m-r-5 text-muted"></i> Settings</a>
+            <a class="dropdown-item" href="#"><i class="mdi mdi-lock-open-outline m-r-5 text-muted"></i> Lock screen</a>
+            <a class="dropdown-item" href="<?php echo base_url(); ?>adminlogin/logout"><i class="mdi mdi-logout m-r-5 text-muted"></i> Logout</a>
+            </div>
+            </li>
+         </ul>
+         <ul class="list-inline menu-left mb-0">
+         <li class="list-inline-item">
+         <button type="button" class="button-menu-mobile open-left waves-effect">
+         <i class="ion-navicon"></i>
+         </button>
+         </li>
+         <li class="hide-phone list-inline-item app-search">
+         <h3 class="page-title">View Events</h3>
+         </li>
+         </ul>
+         <div class="clearfix"></div>
+      </nav>
+      </div>
+    <!-- Top Bar End -->
+
+    <div class="page-content-wrapper ">
+        <div class="container">
+            <div class="row">
+      
+        <div class="col-lg-12">
+            <div class="card m-b-20">
+                <div class="card-block">
+
+                    <h4 class="mt-0 header-title"></h4>
+
+                     <?php if($this->session->flashdata('msg')): ?>
+                        <div class="alert alert-success">
+                           <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                           ×</button> <?php echo $this->session->flashdata('msg'); ?>
+                        </div>
+                        <?php endif; ?>
+
+
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-pills nav-justified" role="tablist" style="width:60%;margin-left:3%;">
+                        <li class="nav-item waves-effect waves-light">
+                            <a class="nav-link active" data-toggle="tab" href="#home-1" role="tab">Advertisement</a>
+                        </li>
+                        <li class="nav-item waves-effect waves-light">
+                            <a class="nav-link" data-toggle="tab" href="#profile-1" role="tab">Hotspot </a>
+                        </li>
+                         <li class="nav-item waves-effect waves-light">
+                            <a class="nav-link" data-toggle="tab" href="#messages-1" role="tab">Normal Events</a>
+                        </li>
+                    </ul>
+
+                    <!-- Tab panes -->
+                    <div class="tab-content">
+                        <div class="tab-pane active p-3" id="home-1" role="tabpanel">
+                            <table  class="table table-striped table-bordered display" cellspacing="0" width="100%">
+                        <thead>
+                        <tr>
+                            <th>Event Name</th>
+                            <th>Event Category</th>
+                            <th>Event City</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach($result as $rows){ 
+                           $adv_sts=$rows->adv_status;
+                          if($adv_sts=='Y'){ ?>
+                        <tr>
+                            <td><?php echo $rows->event_name ; ?></td>
+                            <td><?php echo $rows->category_name ; ?></td>
+                            <td><?php echo $rows->city_name ; ?></td>
+                            <td><a href="<?php echo base_url();?>events/edit_events/<?php echo $rows->id;?>"><i class="fa fa-pencil-square-o"></a></td>
+                        </tr>
+                       <?php } } ?>
+                        </tbody>
+                    </table>
+                        </div>
+
+                        <div class="tab-pane p-3" id="profile-1" role="tabpanel">
+                      <table class="table table-striped table-bordered display" cellspacing="0" width="100%">
+                        <thead>
+                        <tr>
+                            <th>Event Name</th>
+                            <th>Event Category</th>
+                            <th>Event City</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach($result as $rows){ 
+                           $hotspot_sts=$rows->hotspot_status;
+                          if($hotspot_sts=='Y'){ ?>
+                        <tr>
+                            <td><?php echo $rows->event_name ; ?></td>
+                            <td><?php echo $rows->category_name ; ?></td>
+                            <td><?php echo $rows->city_name ; ?></td>
+                            <td><a href="<?php echo base_url();?>events/edit_events/<?php echo $rows->id;?>"><i class="fa fa-pencil-square-o"></a></td>
+                        </tr>
+                       <?php } }  ?>
+                        </tbody>
+                    </table>
+                        </div>
+
+                        <div class="tab-pane p-3" id="messages-1" role="tabpanel">
+                      <table class="table table-striped table-bordered display" cellspacing="0" width="100%">
+                        <thead>
+                        <tr>
+                            <th>Event Name</th>
+                            <th>Event Category</th>
+                            <th>Event City</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach($result as $rows){
+                           $adv_sts=$rows->adv_status;
+                           $hotspot_sts=$rows->hotspot_status;
+                          if($hotspot_sts=='N' && $adv_sts=='N'){ 
+                           ?>
+                        <tr>
+                            <td><?php echo $rows->event_name ; ?></td>
+                            <td><?php echo $rows->category_name ; ?></td>
+                            <td><?php echo $rows->city_name ; ?></td>
+                            <td><a href="<?php echo base_url();?>events/edit_events/<?php echo $rows->id;?>"><i class="fa fa-pencil-square-o"></a></td>
+                        </tr>
+                       <?php } }  ?>
+                        </tbody>
+                    </table>                       
+                        </div>
+                       
+                    </div>
+
+                </div>
+            </div>
+        </div>
+      </div> <!-- end row -->
+
+     </div><!-- container -->
+    </div> <!-- Page content Wrapper -->
+
+
+</div> <!-- content -->
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('table.display').DataTable();
+} );
+</script>
