8462202ae5f497f6967990f650ecef6b66fabec1
devika5595 <devika.happysanz@gmail.com>
2017-11-14 10:55:06 +0530

    delete button removed
    commited

--- application/views/email_template/add_template.php
+++ application/views/email_template/add_template.php
@@ -108,7 +108,7 @@
 								         <td>
                                     <a href="<?php echo base_url();?>emailtemplate/edit_template/<?php echo $rows->id;?>"><img title="Edit" src="<?php echo base_url();?>assets/icons/edit.png" /></a>
 
-                                     <a href="<?php echo base_url();?>emailtemplate/delete_template/<?php echo $rows->id;?>"><img title="Edit" src="<?php echo base_url();?>assets/icons/delete.png" /></a>
+                                     <!--a href="<?php echo base_url();?>emailtemplate/delete_template/<?php echo $rows->id;?>"><img title="Edit" src="<?php echo base_url();?>assets/icons/delete.png" /></a-->
 
                                  </td>
                               </tr>
