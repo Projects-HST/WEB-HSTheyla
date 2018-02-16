fb2a84ee4d2f6b5ac66fc2a00dcb57f1331a9712
kamalhappysanz <kamal.happysanz@gmail.com>
2018-02-16 15:28:00 +0530

    review,wishlist layout
    commited

--- application/views/eventdetails.php
+++ application/views/eventdetails.php
@@ -1,9 +1,11 @@
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
 	<div class="container-fluid eventdetail-pge">
 		<div class="container">
 			<div class="row">
 				<div class="carousel carousel-fade" data-ride="carousel" data-interval="2000">
 					<div class="carousel-inner" role="listbox">
-                    <?php 
+                    <?php
 					//print_r($event_gallery);
 					if (!empty($event_gallery)){
 						$i = 0;
@@ -12,11 +14,11 @@
                             <div class="carousel-item <?php if ($i=='0') echo "active"; ?>">
                                 <img class="d-block w-100" src="<?php echo base_url(); ?>assets/events/gallery/<?php echo $res->event_image; ?>">
                             </div>
-                            
-                      <?php 
-					   $i = $i+1; 
+
+                      <?php
+					   $i = $i+1;
 					   	}
-					  } else { 
+					  } else {
 					  	foreach($event_details as $res){}
 					  ?>
                       		<div class="carousel-item active">
@@ -50,7 +52,16 @@
 								<p class="event-desc-details"><?php echo date('d/m/Y',strtotime($res->start_date));?> <?php echo $res->start_time;?> to <?php echo date('d/m/Y',strtotime($res->end_date)); ?> <?php echo $res->end_time;?></p>
 								<p class="event-desc">Venue</p>
 								<p class="event-desc-details"><b><?php echo $res->event_venue; ?></b> <br /><?php echo $res->event_address; ?></p>
-                               
+								<p class="event-desc">Reviews</p>
+                <p>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star checked"></span>
+                  <span class="fa fa-star"></span>
+                  <span class="fa fa-star"></span>
+
+                  <p><a class="btn btn-login btn-primary btn-block review-btn" data-toggle="modal" data-target="#myModal" data-original-title>Wite a  Review</a> </p>
+                  </p>
 							</div>
 						</section>
 						<section class="row">
@@ -59,11 +70,148 @@
 								<div id="map" class="map"></div>
 							</div>
 						</section>
+						<div class="modal fade modal-lg " id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false" >
+								<div class="modal-dialog">
+										<div class="modal-content">
+											<div class="modal-header">
+												<h4 class="modal-title">Write Review about the Event</h4>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+											</div>
+												<div class="modal-body">
+													<center>
+														<form class="form" role="form" autocomplete="off">
+																<div class="form-group row">
+																		<div class="col-lg-12">
+																				<input class="form-control" type="text" value="" placeholder="Name">
+																		</div>
+																</div>
+																<div class="form-group row">
+																		<div class="col-lg-12">
+																				<input class="form-control" type="text" value="" placeholder="Email">
+																		</div>
+																</div>
+																<div class="form-group row">
+																		<div class="col-lg-12">
+																				 <div class="rating" >
+																						<span class="user-rating">
+																						<input type="radio" name="rating" value="5"><span class="star"></span>
+
+																						    <input type="radio" name="rating" value="4"><span class="star"></span>
+
+																						    <input type="radio" name="rating" value="3"><span class="star"></span>
+
+																						    <input type="radio" name="rating" value="2"><span class="star"></span>
+
+																						    <input type="radio" name="rating" value="1"><span class="star"></span>
+																						</span>
+																					 </div>
+
+																		</div>
+																</div>
+																<div class="form-group row">
+																	<div class="col-lg-12">
+																<div class="g-recaptcha " data-sitekey="6Lf_tUYUAAAAAFhSWPgXhaoCJ-Zlr8ax4rLo-cxE"></div>
+															</div>
+															</div>
+																<div class="form-group row">
+																		<div class="col-lg-12">
+																				<textarea  type="text" value="" placeholder="Message" class="textarea-review"></textarea>
+																		</div>
+																</div>
+																<div class="form-group row">
+																		<div class="col-lg-12">
+																				<input  type="submit" value="Submit Review" placeholder="Message" class="btn btn-primary btn-login">
+																		</div>
+																</div>
+															</form>
+													</center>
+												</div>
+
+										</div>
+								</div>
+						</div>
 					</div>
 				</div>
+
 				<style>
+				.user-rating {
+				    direction: rtl;
+				    font-size: 20px;
+				    unicode-bidi: bidi-override;
+				    padding: 10px 30px;
+				    display: inline-block;
+						width: 500px;
+				}
+				.user-rating input {
+				    opacity: 0;
+				    position: relative;
+				    left: -15px;
+				    z-index: 2;
+				    cursor: pointer;
+				}
+				.user-rating span.star:before {
+				    color: #777777;
+				    content:"ï€†";
+				    /*padding-right: 5px;*/
+				}
+				.user-rating span.star {
+				    display: inline-block;
+				    font-family: FontAwesome;
+				    font-style: normal;
+				    font-weight: normal;
+				    position: relative;
+				    z-index: 1;
+				}
+				.user-rating span {
+				    margin-left: -15px;
+				}
+				.user-rating span.star:before {
+				    color: #777777;
+				    content:"\f006";
+				    /*padding-right: 5px;*/
+				}
+				.user-rating input:hover + span.star:before, .user-rating input:hover + span.star ~ span.star:before, .user-rating input:checked + span.star:before, .user-rating input:checked + span.star ~ span.star:before {
+				    color: #ffd100;
+				    content:"\f005";
+				}
+
+				.selected-rating{
+				    color: #ffd100;
+				    font-weight: bold;
+				    font-size: 3em;
+				}
+
+span.fa.fa-star.checked{
+	color: yellow;
+
+}
+.modal-lg{
+   max-width: 80% !important;
+}
+.modal:before {
+  content: '';
+  display: inline-block;
+  height: 100%;
+  vertical-align: middle;
+  margin-right: -4px;
+}
+.modal-body{
+  padding-top:30px;
+  padding-bottom:30px;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-radius: 20px;
+}
+.modal-dialog {
+  display: inline-block;
+  text-align: left;
+  vertical-align: middle;
+}
+.modal{
+	left: 30%;
+}
 .carousel-fade .carousel-inner, .carousel-fade .carousel-item{
-	height: 400px;
+	height: 387px;
 }
 .carousel{
   width: 100%;
@@ -75,6 +223,9 @@
 }
 </style>
 <script>
+$('#user-rating-form').on('change','[name="rating"]',function(){
+	$('#selected-rating').text($('[name="rating"]:checked').val());
+});
     $('.carousel').carousel({
       interval:4000,
       pause: "false"
---
+++ application/views/review.php
@@ -0,0 +1,153 @@
+<div class="container-fluid page-bg">
+<div class="">
+<div class="row header-title leaderboard-bg">
+  <div class="col-md-12">
+  <div class="container">
+      <p class="leader-title">Bootstrap example of Fixed Background Image using HTML, Javascript, jQuery, and CSS. Snippet by iammahesh.</p>
+    </div>
+  </div>
+</div>
+
+<section class="container">
+  <div class="leaderboard-menu-tab">
+        <div class="row row-offcanvas row-offcanvas-right">
+          <div class="col-12 col-md-3 sidebar-offcanvas" id="sidebar">
+            <div class="list-group">
+              <a href="#" class="list-group-item "><span class="menu-icons"><i class="fas fa-trophy"></i></span>Dashboard</a>
+              <a href="#" class="list-group-item "><span class="menu-icons"><i class="fas fa-user"></i></span>Profile</a>
+              <a href="#" class="list-group-item "><span class="menu-icons"><i class="fas fa-book"></i></span>Booking </a>
+              <a href="#" class="list-group-item "><span class="menu-icons"><i class="fas fa-heart"></i></span>Whishlist</a>
+              <!--a href="<?php echo base_url(); ?>organizerbooking/messageboard/" class="list-group-item">Messages</a-->
+              <a href="#" class="list-group-item active"><span class="menu-icons"><i class="fab fa-wpforms"></i></span>Reviews</a>
+              <a href="#" class="list-group-item"><span class="menu-icons"><i class="fas fa-sign-out-alt"></i></span>Sign Out</a>
+            </div>
+          </div><!--/span-->
+
+          <div class="col-12 col-md-9">
+            <div class="card-header card-header-title">
+                           <h3 class="mb-0">Review</h3>
+            </div>
+              <div class="row">
+                <div class="col-12 event-wish">
+                  <div class="card-group">
+                  <div class="card whishlist-card">
+                    <div class="card-block">
+                    <a href="#">  <h4 class="card-title">Event name</h4></a>
+                    <div class="rating" >
+                       <span class="user-rating">
+                       <input type="radio" name="rating" value="5"><span class="star"></span>
+
+                           <input type="radio" name="rating" value="4"><span class="star"></span>
+
+                           <input type="radio" name="rating" value="3"><span class="star"></span>
+
+                           <input type="radio" name="rating" value="2"><span class="star"></span>
+
+                           <input type="radio" name="rating" value="1"><span class="star"></span>
+                       </span>
+                      </div>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 event-wish">
+                <div class="card-group">
+                <div class="card whishlist-card">
+                  <div class="card-block">
+                  <a href="#">  <h4 class="card-title">Event name</h4></a>
+                  <div class="rating" >
+                     <span class="user-rating">
+                     <input type="radio" name="rating" value="5"><span class="star"></span>
+
+                         <input type="radio" name="rating" value="4"><span class="star"></span>
+
+                         <input type="radio" name="rating" value="3"><span class="star"></span>
+
+                         <input type="radio" name="rating" value="2"><span class="star"></span>
+
+                         <input type="radio" name="rating" value="1"><span class="star"></span>
+                     </span>
+                    </div>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+
+              </div>
+            </div><!--/span-->
+
+        </div><!--/row-->
+   </div>
+</section>
+</div>
+</div>
+<style>
+.user-rating {
+    direction: rtl;
+    font-size: 15px;
+    unicode-bidi: bidi-override;
+    padding: 0px 14px;
+    display: inline-block;
+
+}
+.user-rating input {
+    opacity: 0;
+    position: relative;
+    left: -15px;
+    z-index: 2;
+    cursor: pointer;
+}
+.user-rating span.star:before {
+    color: #777777;
+    content:"ï€†";
+    /*padding-right: 5px;*/
+}
+.user-rating span.star {
+    display: inline-block;
+    font-family: FontAwesome;
+    font-style: normal;
+    font-weight: normal;
+    position: relative;
+    z-index: 1;
+}
+.user-rating span {
+    margin-left: -15px;
+}
+.user-rating span.star:before {
+    color: #777777;
+    content:"\f006";
+    /*padding-right: 5px;*/
+}
+.user-rating input:hover + span.star:before, .user-rating input:hover + span.star ~ span.star:before, .user-rating input:checked + span.star:before, .user-rating input:checked + span.star ~ span.star:before {
+    color: #ffd100;
+    content:"\f005";
+}
+
+.selected-rating{
+    color: #ffd100;
+    font-weight: bold;
+    font-size: 3em;
+}
+.form-group{
+  margin-bottom: 0px;
+}
+.list-group-item{
+  border: none;
+  color: #000;
+}
+body{
+  background-color: #f6f6f6;
+}
+</style>
+<script>
+
+</script>
