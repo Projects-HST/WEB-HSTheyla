80ba0f3ea939396ce6941be4e16bfd035161561f
senmaran <maran.happysanz@gmail.com>
2018-02-27 18:05:51 +0530

    Wishlist update
    Wishlist update

--- application/models/Loginmodel.php
+++ application/models/Loginmodel.php
@@ -614,7 +614,7 @@ Class Loginmodel extends CI_Model
 	
 		public function get_wishlist($user_id)
 	{
-		$sql="select ev.*,uwl.updated_at as wl_updated_at from events as ev LEFT JOIN user_wish_list as uwl on uwl.event_id = ev.id WHERE uwl.user_id = '85' group by ev.id ORDER BY uwl.updated_at desc";
+		$sql="select ev.*,uwl.updated_at as wl_updated_at from events as ev LEFT JOIN user_wish_list as uwl on uwl.event_id = ev.id WHERE uwl.user_id = '$user_id' group by ev.id ORDER BY uwl.updated_at desc";
 		$resu=$this->db->query($sql);
 		$res=$resu->result();
 		return $res;
