97174215505f7e7855f77301d23d8788e862e639
senmaran <maran.happysanz@gmail.com>
2018-03-02 12:24:25 +0530

    attendees changes
    attendees changes

--- application/views/event_attendees.php
+++ application/views/event_attendees.php
@@ -1,5 +1,4 @@
-<div class="container-fluid signinbg">
-    <?php
+ <?php
         foreach($seats as $rows) {
          $number_of_seats = $rows->number_of_seats;
          $order_id = $rows->order_id; 
@@ -7,38 +6,98 @@
          $email_id = $rows->email_id; 
          $mobile_no = $rows->mobile_no; 
         }
-    ?>
+?>
+<div class="container-fluid page-bg">
+  <div class="row header-title attendees-bg">
+    <div class="col-md-12">
+    <div class="container">
+        <p class="add-attendees">Add Attendees</p>
+      </div>
+      <div class="container attendees-form">
     <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-6 col-md-auto signin-div" style="margin-bottom:100px;">
-            <form id="attendees" name="attendees" method="post" action="<?php echo base_url(); ?>home/insertattendees" onsubmit="return check();">
-            <?php
+      <div class="col-md-12">
+       
+        <form id="attendees" name="attendees" class="form-inline" method="post" action="<?php echo base_url(); ?>home/insertattendees" onsubmit="return check();">
+        
+        
+         <?php
                 for ($i=1; $i < $number_of_seats+1; $i++) {
                     if ($i=='1'){
             ?>
-                    <h4>Event attendees <?php echo $i; ?></h4>
-                    <input type="text" name="name<?php echo $i;?>" value="<?php echo $name; ?>">
-                    <input type="text" name="email<?php echo $i;?>" value="<?php echo $email_id; ?>">
-                    <input type="text" name="phone<?php echo $i;?>" value="<?php echo $mobile_no; ?>">
+              <div class="row formrow">
+                  <div class="col-md-4">
+                  <label for="exampleInputEmail3" class="label-form">Name</label>
+                    <input type="text" class="form-control" name="name<?php echo $i;?>" value="<?php echo $name; ?>">
+                  </div>
+                  <div class="col-md-4">
+                  <label for="exampleInputEmail3" class="label-form">Email Id</label>
+                    <input type="text" class="form-control" name="email<?php echo $i;?>" value="<?php echo $email_id; ?>">
+                  </div>
+                  <div class="col-md-4">
+                  <label for="exampleInputEmail3" class="label-form">Phone Number</label>
+                    <input type="text" class="form-control" name="email<?php echo $i;?>" value="<?php echo $mobile_no; ?>">
+                  </div>
+               </div>
             <?php
                     } else {
             ?>
-                    <h4>Event attendees <?php echo $i; ?></h4>
-                    <input type="text" name="name<?php echo $i;?>" value="">
-                    <input type="text" name="email<?php echo $i;?>" value="">
-                    <input type="text" name="phone<?php echo $i;?>" value="">
+                <div class="row formrow">
+                  <div class="col-md-4">
+                  <label for="exampleInputEmail3" class="label-form">Name</label>
+                    <input type="text" class="form-control" name="name<?php echo $i;?>" value="">
+                  </div>
+                  <div class="col-md-4">
+                  <label for="exampleInputEmail3" class="label-form">Email Id</label>
+                    <input type="text" class="form-control" name="email<?php echo $i;?>" value="">
+                  </div>
+                  <div class="col-md-4">
+                  <label for="exampleInputEmail3" class="label-form">Phone Number</label>
+                    <input type="text" class="form-control" name="email<?php echo $i;?>" value="">
+                  </div>
+               </div>
+
             <?php
                     }
                 }
             ?>
-            <input type="hidden" name="order_id" id="order_id" value="<?php echo $order_id; ?>" />
+         	<input type="hidden" name="order_id" id="order_id" value="<?php echo $order_id; ?>" />
             <input type="hidden" name="count" id="count" value="<?php echo $number_of_seats; ?>" />
             <input type="submit" name="button" id="button" value="Submit" />
-            </form>
-        </div>
-    	<div class="col-md-3"></div>
-	</div>
+         </form>
+      </div>
+    </div>
+  </div>
+
+    </div>
+  </div>
+
 </div>
+<style>
+#stickfooter{
+  position: absolute;
+  width: 100%;
+  bottom: 0px;
+}
+
+label {
+  margin-bottom: 0;
+  margin-left: 1px;
+}
+.form-inline .form-control{
+  width: 100%;
+}
+.formrow{
+  width: 100%;
+}
+body{
+  background-color:  #eeeeee;
+
+}
+</style>
+
+
+
+
 <script language="javascript" type="text/javascript">
 function check()
 {
