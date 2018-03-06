d4e8bfa7450faaf1140bbe1ae7b1c25a824f3850
kamalhappysanz <kamal.happysanz@gmail.com>
2018-03-06 17:06:46 +0530

    payment error
    committed

--- application/views/payment_error.php
+++ application/views/payment_error.php
@@ -2,9 +2,9 @@
     <div class="row">
       <div class="container">
         <div class="col-md-12 payment_msg">
-          <center><h1>Thank you.</h1>
-            <p class="payment_success">Your payment has been received successfully. For other details regarding the payment visit your booking details page. We have sent an email to you with order details. If you have queries contact us.</p>
-            <img src="<?php  echo base_url(); ?>assets/front/images/success.png">
+          <center><h1>Payment Error</h1>
+            <p class="payment_success">Your payment was unsuccessful. Please try again. If you continue to receive an error message please contact us.</p>
+            <img src="<?php  echo base_url(); ?>assets/front/images/cancel.png">
           </center>
         </div>
       </div>
