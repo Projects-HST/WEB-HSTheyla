b872cf586e5dbdb08f9fe7f36880a16b8d4da679
Maran <maran.happysanz@gmail.com>
2019-08-08 10:13:09 +0530

    Seat check
    Seat check

--- application/models/Eventlistmodel.php
+++ application/models/Eventlistmodel.php
@@ -522,7 +522,7 @@ Class Eventlistmodel extends CI_Model
 				$seatsupdate = $this->db->query($update_seats);
 
 				$_SESSION['booking_start'] = time(); // taking now logged in time
-				$_SESSION['booking_expire'] = $_SESSION['booking_start'] + (900) ; // ending a session in 10mins
+				$_SESSION['booking_expire'] = $_SESSION['booking_start'] + (300) ; // ending a session in 10mins
 
 				$session_seats = "INSERT INTO booking_session (session_expiry,order_id,plan_time_id,number_of_seats,status) VALUES ('". $_SESSION['booking_expire'] . "','". $order_id . "','". $plan_time_id . "','". $number_of_seats . "','Start')";
 				$session_insert = $this->db->query($session_seats);
