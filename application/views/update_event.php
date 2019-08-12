91ea4a7f344f4ad3b7213ca510dda242d2384b9f
Maran <maran.happysanz@gmail.com>
2019-08-12 13:04:44 +0530

    update event changes
    update event changes

--- application/views/update_event.php
+++ application/views/update_event.php
@@ -104,7 +104,7 @@ function get_times( $default = '10:00', $interval = '+15 minutes' )
 				 <label for="Address" class="col-sm-2 col-form-label">Address</label>
 				<div class="col-sm-4">
 			<textarea id="textarea" name="address" required="" class="form-control" maxlength="240" rows="3" placeholder="">
-            <?php echo $rows->event_address; ?></textarea>
+            <?php echo trim($rows->event_address); ?></textarea>
 				</div>
 				
 				<label for="Description" class="col-sm-2 col-form-label">Description</label>
