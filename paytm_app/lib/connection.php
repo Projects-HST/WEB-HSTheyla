87c3986c0f6e5f6211de0ca8365b2a45f56536b0
Maran <maran.happysanz@gmail.com>
2019-08-31 11:07:22 +0530

    db changes
    db changes

--- paytm_app/lib/connection.php
+++ paytm_app/lib/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = @mysql_connect("localhost","heylaapp_apptest","heyla@66444");
+$con = @mysql_connect("localhost","heylaapp_app","heyla@66444");
 
 if ($con) {
 		mysql_select_db('heylaapp_apptest');
