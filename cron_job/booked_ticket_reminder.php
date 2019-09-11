8bd06a2eeafda8d340865406cb947ce1811fb473
Maran <maran.happysanz@gmail.com>
2019-09-11 09:47:23 +0530

    cronjob checking
    cronjob checking

--- cron_job/booked_ticket_reminder.php
+++ cron_job/booked_ticket_reminder.php
@@ -5,7 +5,7 @@ $current_time = date("h:i A", time());
 $con = @mysql_connect("localhost","root","O+E7vVgBr#{}");
 
 if ($con) {
-		mysql_select_db('heylaapp_apptest');
+		mysql_select_db('heylaapp_app');
     } else {
 		die("Connection failed");
 }
--- cron_job/seat_session_check.php
+++ cron_job/seat_session_check.php
@@ -3,7 +3,7 @@
 $con = @mysql_connect("localhost","root","O+E7vVgBr#{}");
 
 if ($con) {
-		mysql_select_db('heylaapp_apptest');
+		mysql_select_db('heylaapp_app');
     } else {
 		die("Connection failed");
 }
