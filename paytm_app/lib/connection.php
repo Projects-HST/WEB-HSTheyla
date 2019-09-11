83120fa91fd4d5cd5da51777558b34b10051b9a2
Maran <maran.happysanz@gmail.com>
2019-09-11 09:57:56 +0530

    connection changes
    connection changes

--- paytm_app/lib/connection.php
+++ paytm_app/lib/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$con = @mysql_connect("localhost","heylaapp_app","heyla@66444");
+$con = @mysql_connect("localhost","root","O+E7vVgBr#{}");
 
 if ($con) {
-		mysql_select_db('heylaapp_apptest');
+		mysql_select_db('heylaapp_app');
     } else {
 		die("Connection failed");
 }
