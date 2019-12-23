fc1c38e39be471f22d5806f587e339d14d15a900
Maran <maran.happysanz@gmail.com>
2019-12-23 16:38:46 +0530

    date time changes
    date time changes

--- paytm_app/TxnStatus.php
+++ paytm_app/TxnStatus.php
@@ -1,4 +1,5 @@
 <?php
+	date_default_timezone_set('Asia/Kolkata');
     require_once("./lib/connection.php");
     
 		$current_time = time(); 
--- paytm_web/TxnStatus.php
+++ paytm_web/TxnStatus.php
@@ -1,4 +1,5 @@
 <?php
+	date_default_timezone_set('Asia/Kolkata');
 	header("Pragma: no-cache");
 	header("Cache-Control: no-cache");
 	header("Expires: 0");
