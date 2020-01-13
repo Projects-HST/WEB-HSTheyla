c68356d72914b9118dd32f7e042d6119e2d54776
Maran <maran.happysanz@gmail.com>
2020-01-13 15:59:37 +0530

    Update edit_events.php

--- application/views/events/edit_events.php
+++ application/views/events/edit_events.php
@@ -261,7 +261,7 @@
    window.onload = function () {
     var mapOptions = {
                 center: new google.maps.LatLng(<?php echo $rows->event_latitude; ?>, <?php echo $rows->event_longitude; ?>),
-                zoom:8,
+                zoom:10,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
             var infoWindow = new google.maps.InfoWindow();
