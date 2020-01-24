99ab108745a3dffa1dfd3a790cf17931d1ca80ef
Maran <maran.happysanz@gmail.com>
2020-01-24 13:15:11 +0530

    sms changes
    sms changes

--- application/models/Smsmodel.php
+++ application/models/Smsmodel.php
@@ -41,13 +41,12 @@ Class Smsmodel extends CI_Model
 
 		$response = curl_exec($curl);
 		$err = curl_error($curl);
-
 		curl_close($curl);
 
 		if ($err) {
 		echo "cURL Error #:" . $err;
 		} else {
-		//echo $response;
+			echo $response;
 		}
 
 	}
